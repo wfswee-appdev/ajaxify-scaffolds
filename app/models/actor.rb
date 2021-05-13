d2e646efe3239149465e0fa7af92c5dfa70cb327
wfswee <wfswee@chicagobooth.edu>
2021-05-13 20:31:29 +0000

    installed annotate

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: actors
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  bio        :text
+#  dob        :date
+#  image      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Actor < ApplicationRecord
+
 end
